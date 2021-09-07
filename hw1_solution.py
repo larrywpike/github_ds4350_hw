838f2ff666aa98045429d55f6a454cc6623c5c7f
larrywpike <l.pike@comcast.net>
2021-09-07 13:51:06 -0600

    added hw1_solution.py for first time

---
+++ hw1_solution.py
@@ -0,0 +1,13 @@
+"""!
+   @brief File for a DS4350 Homework #1
+   
+Created on Sept 7, 2021
+
+@author: larrypike
+"""
+
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('/Users/larrypike/Dropbox/machine_learning/code')
+import ds_module  as dsm
